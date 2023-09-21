7d420559724876ac2593e4fad4f7a65568902566
FallenUns <96117344+FallenUns@users.noreply.github.com>
2023-09-21 21:01:44 +1000

    Removing Question 2 Field

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Function to determine season based on date
 function getSeason(date) {
   const month = date.getMonth();
   if (month >= 2 && month <= 4) return 'Autumn';
@@ -7,15 +6,12 @@ function getSeason(date) {
   return 'Summer';
 }
 
-// Initialize DOM Elements
 const dateElement = document.getElementById('holidayDate');
-const activityElement = document.getElementById('activity');
 const recommendationElement = document.getElementById('recommendation');
 const weatherIcon = document.getElementById('weatherIcon');
 const sunIcon = document.getElementById('sunIcon');
 const sunInfo = document.getElementById('sunTime');
 
-// Activity options for each season
 const activities = {
   'Summer': ['Beach', 'Camping', 'Wildlife'],
   'Autumn': ['Wine Tasting', 'Scenic Drives', 'River Cruise'],
@@ -23,41 +19,34 @@ const activities = {
   'Spring': ['Hiking Trail', 'Sport Events', 'Outdoor Dining']
 };
 
-// Weather API Key and Melbourne coordinates
 const apiKey = 'f367485e723c43718aaaf6d17ecf21f3';
 const melbourneLat = -37.8136;
 const melbourneLon = 144.9631;
 
-// Fetch weather data
 function fetchWeatherData() {
   const apiUrl = `https://pro.openweathermap.org/data/2.5/weather?lat=${melbourneLat}&lon=${melbourneLon}&appid=${apiKey}`;
   fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
     let iconClass;
-    // Convert temperature to Celsius and round it
     const temperatureInKelvin = data.main.temp;
     const temperatureInCelsius = parseInt(temperatureInKelvin - 273.15, 10);
 
-    // Update temperature
     const temperatureValue = document.getElementById('temperatureValue');
     if (temperatureValue) {
       temperatureValue.innerHTML = `${temperatureInCelsius}°C`;
     }
 
-    // Update location information
     const locationInfo = document.getElementById('locationInfo');
     if (locationInfo) {
       locationInfo.innerHTML = "Melbourne Weather Right Now";
     }
 
-    // Fetch and convert sunrise and sunset times
     const sunriseTimestamp = data.sys.sunrise;
     const sunsetTimestamp = data.sys.sunset;
     const sunrise = new Date(sunriseTimestamp * 1000);
     const sunset = new Date(sunsetTimestamp * 1000);
 
-    // Compare with current time
     const currentTime = new Date();
     let sunEvent, sunTime, sunIconClass;
   
@@ -71,13 +60,12 @@ function fetchWeatherData() {
       sunIconClass = "wi-sunset";
     } else {
       sunEvent = "Sunrise";
-      sunTime = sunrise; // For the next day
+      sunTime = sunrise;
       sunIconClass = "wi-sunrise";
     }
 
-    // Weather Condition
     const conditionCode = data.weather[0].id;
-    const dayOrNight = data.weather[0].icon.slice(-1); // 'd' for day, 'n' for night
+    const dayOrNight = data.weather[0].icon.slice(-1);
 
     if (conditionCode >= 200 && conditionCode < 300) {
       iconClass = 'wi-thunderstorm';
@@ -100,7 +88,7 @@ function fetchWeatherData() {
     } else if (conditionCode >= 803 && conditionCode <= 804) {
       iconClass = 'wi-cloudy';
     } else {
-      iconClass = 'wi-na';  // Not available
+      iconClass = 'wi-na';
     }
 
     const weatherIcon = document.getElementById('weatherIcon');
@@ -108,11 +96,9 @@ function fetchWeatherData() {
       weatherIcon.className = `wi ${iconClass}`;
     }
 
-    // Update the sun icon
     const sunIcon = document.getElementById('sunIcon');
     sunIcon.className = `wi ${sunIconClass}`;
 
-    // Update the sun information
     const sunInfo = document.getElementById('sunTime');
     const formattedTime = `${sunTime.getHours()}:${sunTime.getMinutes()}`;
     sunInfo.innerHTML = `${formattedTime}`;
@@ -122,7 +108,6 @@ function fetchWeatherData() {
   });
 }
 
-// Colors for each season
 const colors = {
   'Summer': '#FACE5C',
   'Autumn': '#EA4D2A',
@@ -130,7 +115,6 @@ const colors = {
   'Spring': '#F59AE7'
 };
 
-// Symbols for each season
 const seasonSymbols = {
   'Summer': '☀️',
   'Autumn': '🍁',
@@ -138,35 +122,18 @@ const seasonSymbols = {
   'Spring': '🌸'
 };
 
-// Event Listener to update activities based on selected date
 dateElement.addEventListener('change', function() {
   const selectedDate = new Date(dateElement.value);
   const selectedSeason = getSeason(selectedDate);
   const seasonActivities = activities[selectedSeason];
   const selectedColor = colors[selectedSeason] || '#FFFFFF';
 
-  // Update activity options based on season
-  activityElement.innerHTML = seasonActivities.map(activity => `<option value="${activity}">${activity}</option>`).join('');
-
-  // Update background color based on season
-  activityElement.style.backgroundColor = selectedColor;
-
-  // Update text color based on background
-  if (selectedColor === '#2145C7' || selectedColor === '#EA4D2A') {
-    activityElement.style.color = '#FFFFFF';
-  } else {
-    activityElement.style.color = '#000000';
-  }
-
-  // Update the text field with the current season
   const seasonField = document.getElementById('currentSeason');
   seasonField.innerHTML = ` Selected Season: ${selectedSeason}`;
 
-  // Update the background color of the season box
   const seasonBox = document.getElementById('seasonBox');
   seasonBox.style.backgroundColor = selectedColor;
 
-  // Update text color based on background
   if (selectedColor === '#2145C7' || selectedColor === '#EA4D2A') {
     seasonBox.style.color = '#FFFFFF';
   } else {
@@ -177,15 +144,12 @@ dateElement.addEventListener('change', function() {
   seasonField.innerHTML = `Current Season: ${selectedSeason} ${selectedSeasonSymbol}`;
 });
 
-// Function to get recommendations
 function getRecommendation() {
   const selectedDate = new Date(dateElement.value);
   const selectedSeason = getSeason(selectedDate);
-  const selectedActivity = activityElement.value;
-  const recommendation = `Place for ${selectedActivity} during ${selectedSeason}`;
+  const recommendation = `Place for ${selectedSeason}`;
   recommendationElement.innerText = recommendation;
 }
 
-// Initialize
 dateElement.dispatchEvent(new Event('change'));
 fetchWeatherData();
